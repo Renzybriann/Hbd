25c81bf2fb81a84a430d9a96a52685d6a6861fa7
Renzybriann <youngrenzybrian@gmail.com>
2024-08-19 23:17:03 +0800

    hbd

--- src/CoffeeCup.jsx
+++ src/CoffeeCup.jsx
@@ -3,6 +3,7 @@ import { Howl } from 'howler';
 import Confetti from 'react-confetti';
 import coffeeCupImage from './assets/coffee-cup.png'; // Adjust path if needed
 import clickSound from './sounds/click.mp3'; // Adjust path if using public folder
+import balloon from './assets/balloon.png'
 
 const sound = new Howl({
   src: [clickSound],
@@ -20,12 +21,13 @@ function CoffeeCup() {
   };
 
   return (
-    <div className="relative flex flex-col items-center justify-center h-screen bg-gray-200">
+    <div className="relative flex flex-col items-center justify-center h-screen bg-gray-200 overflow-hidden">
       {showMessage && <Confetti />}
       {!showMessage && (
         <>
-          <div className="text-xl font-bold mb-4 animate-fadeIn">
-          Mahilig ka ba sa kape? <br/>Kung oo pindutin mo yung kape
+          <div className="text-xl font-bold mb-4 animate-fadeIn text-center">
+            <div>Mahilig ka ba sa kape?</div> 
+            <div>Kung oo pindutin mo yung kape</div>
           </div>
           <div className="relative flex items-center justify-center cursor-pointer" onClick={handleClick}>
             <img
@@ -38,9 +40,21 @@ function CoffeeCup() {
       )}
       {showMessage && (
         <div className="text-9xl font-bold text-center animate-bounce animate-fadeIn">
-          Happy Birthday <span className="text-blue-600">Kier</span>
+          <div>Happy Birthday</div> 
+          <div className="text-[200px] mt-9 rainbow-text">
+            Jalleah!!!
+          </div>
+           {/* Balloons */}
+      <div className="absolute top-40 -left-52 z-10">
+        <img src={balloon} alt="Balloon" className="w-80 h-80 " />
+      </div>
+      <div className="absolute top-40 -right-52 z-10">
+        <img src={balloon} alt="Balloon" className="w-80 h-80 " />
+      </div>
         </div>
+        
       )}
+     
     </div>
   );
 }
